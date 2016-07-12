56203bc361a730cd9252a7012227f941f4ac5e44
Alyson Takai <shtakai@gmail.com>
2016-07-12 13:48:00 -0700

    index

--- server.js
+++ server.js
@@ -8,6 +8,68 @@ const app = express();
 
 
 
+mongoose.connect('mongodb://localhost/dojo_message_board_development');
+
+// Schema
+const MessageSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 4
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Comment'
+    }
+  ]
+},{
+  timestamps: true
+});
+
+const CommentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 4
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  _message: {
+    type: Schema.Types.ObjectId,
+    ref: 'Message'
+  }
+},{
+  timestamps: true
+});
+
+// register
+mongoose.model('Message', MessageSchema );
+mongoose.model('Comment', CommentSchema);
+
+
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+
+
+
+app.get('/', function(req, res){
+  console.log('get /');
+  res.render('index');
+})
+
+app.post('/messages', function(req, res){
+  console.log('post /messages');
+  console.log('body', req.body);
+  res.send('---');
+})
 
 
 
